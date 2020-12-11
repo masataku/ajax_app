ddecf2420564762c53d3675e3bc77978e958bf0b
masataka_omura <masataka.omura@icloud.com>
2020-12-11 12:26:17 +0900

    メモ投稿機能のフロントエンド実装

--- app/javascript/checked.js
+++ app/javascript/checked.js
@@ -1,9 +1,9 @@
 function check () {
   const posts = document.querySelectorAll(".post");
   posts.forEach (function (post) {
-    // if (post.getAttribute("data-load") != null) {
-    //   return null;
-    // }
+    if (post.getAttribute("data-load") != null) {
+      return null;
+    }
     post.setAttribute("data-load", "true");
     post.addEventListener("click", () => {
       const postId = post.getAttribute("data-id");
